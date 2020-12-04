c4689cdd2a7a3e9a8d88b917569ce7012fc03c15
MarcosNBJ <marcosnery.comp@gmail.com>
2020-12-03 23:53:05 -0300

    better commenting scheduling function

--- web/rated_movies_scheduling.py
+++ web/rated_movies_scheduling.py
@@ -2,64 +2,88 @@ import bisect
 
 
 class RatedMoviesScheduling(object):
-    def __init__(self, I):
-        # (key = lambda tup : tup[1])
-        self.I = sorted(I, key=lambda tup: tup[1])
+    def __init__(self, movies):
+        
+        # ordering our movies by finish time 
+        self.movies = sorted(movies, key=lambda tup: tup[1])
+
+        #initialinzg optimal solution as empty, used for memorization
         self.OPT = []
         self.solution = []
 
-    def previous_intervals(self):
-        start = [task[0] for task in self.I]
-        finish = [task[1] for task in self.I]
-        p = []
-
-        for i in range(len(self.I)):
-            # finds idx for which to input start[i] in finish times to still be
-            # sorted
-            idx = bisect.bisect(finish, start[i]) - 1
-            p.append(idx)
-
-        return p
 
     def find_solution(self, j):
+        '''
+        Since we need to know which movies were selected
+        here we have the post-processing responsible for doing that backtracking
+        '''
+
+        # base case
         if j == -1:
             return
 
+        # find what movie was selected
         else:
-            if (self.I[j][2] + self.compute_opt(self.p[j])
+            if (self.movies[j][2] + self.compute_opt(self.p[j])
                     ) > self.compute_opt(j - 1):
-                self.solution.append(self.I[j])
+                self.solution.append(self.movies[j])
                 self.find_solution(self.p[j])
 
             else:
                 self.find_solution(j - 1)
 
+    def previous_intervals(self):
+        '''
+        Responsible for deciding between orverlapping movies
+        '''
+        start = [movie[0] for movie in self.movies]
+        finish = [movie[1] for movie in self.movies]
+        p = []
+
+        for i in range(len(self.movies)):
+ 
+            # here we use bisect to find in which index we have to append the movie 
+            # in order for the list to mantaint its ordination
+
+            idx = bisect.bisect(finish, start[i]) - 1
+            p.append(idx)
+
+        return p
+
     def compute_opt(self, j):
+
+        # base case
         if j == -1:
             return 0
-
+        
+        # that means we've already calculated OPT for that case, so we just use it
         elif (0 <= j) and (j < len(self.OPT)):
             return self.OPT[j]
 
+        # calculating the optimal solution
         else:
-            return max(
-                self.I[j][2] +
-                self.compute_opt(
-                    self.p[j]),
-                self.compute_opt(
-                    j -
-                    1))
+            return max(self.movies[j][2] + self.compute_opt(self.p[j]),
+                self.compute_opt(j -1))
 
     def weighted_interval(self):
-        if len(self.I) == 0:
+        '''
+        Calculates the optimal schedule
+        Returns the resulting weight and the movies selected
+        '''
+
+        # base case
+        if len(self.movies) == 0:
             return 0, self.solution
 
+        # calculating everything for all movies
         self.p = self.previous_intervals()
 
-        for j in range(len(self.I)):
+        for j in range(len(self.movies)):
             opt_j = self.compute_opt(j)
             self.OPT.append(opt_j)
 
-        self.find_solution(len(self.I) - 1)
-
+        self.find_solution(len(self.movies) - 1)
+        
+        # here we reverse the order of the solution because 
+        # the backtracking returns the selected movies in reverse order
         return self.OPT[-1], self.solution[::-1]
